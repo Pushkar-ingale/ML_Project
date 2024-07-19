10c8a55c1d64cde09e8c9135d1e9b576b67dac40
Pushkar-ingale <pushkaringale358@gmail.com>
2024-07-20 00:52:52 +0530

    nothing

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -17,9 +17,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
         
-if __name__=='__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Custom Exception Raised")
-        raise CustomException(e,sys)
